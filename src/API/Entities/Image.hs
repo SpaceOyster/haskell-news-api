ecb0070092319c80e26c1cd9163173c6ca2966e6
SpaceOyster <spaceoyster@disroot.org>
2022-08-22 00:41:57 +0500

    added API.Entities.Image data type

---
+++ src/API/Entities/Image.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module API.Entities.Image where
+
+import Data.ByteString
+import Data.Int
+import Data.Text
+import Database.Beam
+
+data ImageT f = Image
+  { _imageId :: Columnar f Int32,
+    _imageFileExtension :: Columnar f Text,
+    _imageContent :: Columnar f ByteString
+  }
+  deriving (Generic, Beamable)
+
+type Image = ImageT Identity
+
+deriving instance Show Image
+
+deriving instance Eq Image
+
+instance Table ImageT where
+  data PrimaryKey ImageT f = ImageId (Columnar f Int32)
+    deriving (Generic, Beamable)
+  primaryKey = ImageId . _imageId
+
+type ImageId = PrimaryKey ImageT Identity
+
+deriving instance Show (PrimaryKey ImageT Identity)
+
+deriving instance Eq (PrimaryKey ImageT Identity)
