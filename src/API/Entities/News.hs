6bc0d967946d13b09cd1050e39e4b81025eefbfa
SpaceOyster <spaceoyster@disroot.org>
2022-08-22 00:42:22 +0500

    added API.Entities.News data type

---
+++ src/API/Entities/News.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module API.Entities.News where
+
+import API.Entities.Category
+import API.Entities.Image
+import API.Entities.User
+import Data.Int
+import Data.Set
+import Data.Text
+import Data.Time.Clock
+import Database.Beam
+
+data ArticleT f = Article
+  { _articleId :: Columnar f Int32,
+    _articleTitle :: Columnar f Text,
+    _articleCreatedAt :: Columnar f UTCTime,
+    _articleAuthor :: PrimaryKey UserT f,
+    _articleCategory :: PrimaryKey CategoryT f,
+    _articleBody :: Columnar f Text,
+    _articleImages :: PrimaryKey ImageT f,
+    _articleIsPublished :: Columnar f Bool
+  }
+  deriving (Generic, Beamable)
+
+type Article = ArticleT Identity
+
+deriving instance Show Article
+
+deriving instance Eq Article
+
+instance Table ArticleT where
+  data PrimaryKey ArticleT f = ArticleId (Columnar f Int32)
+    deriving (Generic, Beamable)
+  primaryKey = ArticleId . _articleId
+
+type ArticleId = PrimaryKey ArticleT Identity
+
+deriving instance Show (PrimaryKey ArticleT Identity)
+
+deriving instance Eq (PrimaryKey ArticleT Identity)
