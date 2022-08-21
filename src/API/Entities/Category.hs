bf718300059a56c4da3756c6cc67ebd774c38f6c
SpaceOyster <spaceoyster@disroot.org>
2022-08-22 00:40:45 +0500

    added API.Entities.Category data type

---
+++ src/API/Entities/Category.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module API.Entities.Category where
+
+import Data.Set
+import Data.Text
+import Database.Beam
+
+data CategoryT f = Category
+  { _categoryName :: Columnar f Text,
+    _categoryParentCategory :: Columnar f (Maybe Text)
+  }
+  deriving (Generic, Beamable)
+
+type Category = CategoryT Identity
+
+deriving instance Show Category
+
+deriving instance Eq Category
+
+instance Table CategoryT where
+  data PrimaryKey CategoryT f = CategoryId (Columnar f Text)
+    deriving (Generic, Beamable)
+  primaryKey = CategoryId . _categoryName
+
+type CategoryId = PrimaryKey CategoryT Identity
+
+deriving instance Show (PrimaryKey CategoryT Identity)
+
+deriving instance Eq (PrimaryKey CategoryT Identity)
