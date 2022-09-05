b87d860537e55f1bd4f2df0eb7cb4ec364de3e63
SpaceOyster <spaceoyster@disroot.org>
2022-09-05 18:23:10 +0500

    added config parsing with `configurator`

--- src/App/Config.hs
+++ src/App/Config.hs
@@ -1,5 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module App.Config where
 
+import Control.Monad.Catch
+import Control.Monad.IO.Class
+import qualified Data.Configurator as C
+import qualified Data.Configurator.Types as C
 
 newtype AppConfig = AppConfig {serverConfig :: ServerConfig}
   deriving (Show)
@@ -8,3 +15,17 @@ newtype ServerConfig = ServerConfig
   { port :: Integer
   }
   deriving (Show)
+
+readConfigFromFile :: (MonadIO m, MonadFail m) => FilePath -> m AppConfig
+readConfigFromFile cfgPath =
+  liftIO $ C.load [C.Required cfgPath] >>= toAppConfig
+
+toAppConfig :: C.Config -> IO AppConfig
+toAppConfig cfg = do
+  serverConfig <- toServerConfig $ C.subconfig "API" cfg
+  return $ AppConfig {..}
+
+toServerConfig :: C.Config -> IO ServerConfig
+toServerConfig cfg = do
+  port <- C.require cfg "port"
+  return $ ServerConfig {..}
