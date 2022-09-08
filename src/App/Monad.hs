6ab6888cf16f6066bdc447a2bb842e932adb0141
SpaceOyster <spaceoyster@disroot.org>
2022-09-08 20:53:02 +0500

    added MonadDatabase instance for App.Monad

--- src/App/Monad.hs
+++ src/App/Monad.hs
@@ -4,11 +4,13 @@
 
 module App.Monad where
 
-import App.Env (Env (envLogger))
+import App.Env (Env (envDatabase, envLogger))
 import Control.Monad.Catch (MonadCatch, MonadThrow)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Reader (MonadReader, ReaderT (..), asks)
+import Effects.Database as DB
 import Effects.Log as Log
+import qualified Handlers.Database as DB
 import Handlers.Logger as Logger
 import Servant (Handler)
 
@@ -35,3 +37,9 @@ instance Log.MonadLog App where
     hLog <- asks envLogger
     let logAction = Logger.getLog hLog
     App . liftIO $ logAction p t
+
+instance DB.MonadDatabase App where
+  runQuery q = do
+    hDB <- asks envDatabase
+    let runQ = DB.runDBQuery hDB
+    App . liftIO $ runQ q
