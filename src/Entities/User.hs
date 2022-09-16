21994e8a215fc4a46b4d04ccb362420db37e034e
SpaceOyster <spaceoyster@disroot.org>
2022-09-16 22:33:16 +0500

    added Entities.User.generateHash - randomised hashing function

--- src/Entities/User.hs
+++ src/Entities/User.hs
@@ -1,16 +1,20 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Entities.User where
 
+import qualified Crypto.KDF.PBKDF2 as Crypto
+import qualified Crypto.Random as CRand
 import qualified Data.ByteString as BS
 import Data.Int
 import Data.Text
 import Data.Time.Clock
 import Database.Beam
+import qualified System.Random as Rand
 
 data UserT f = User
   { _userId :: Columnar f Int32,
@@ -48,3 +52,17 @@ data PasswordHash = PasswordHash
     _passwordHashSalt :: BS.ByteString
   }
   deriving (Show)
+
+generateHash :: MonadIO m => BS.ByteString -> m PasswordHash
+generateHash pwd = liftIO $ do
+  hashLength <- Rand.getStdRandom $ Rand.randomR (32, 64)
+  hashSaltLength <- Rand.getStdRandom $ Rand.randomR (8, 32)
+  _passwordHashIterations <- Rand.getStdRandom $ Rand.randomR (1000, 4000)
+  let params =
+        Crypto.Parameters
+          { iterCounts = fromIntegral _passwordHashIterations,
+            outputLength = hashLength
+          }
+  _passwordHashSalt <- CRand.getRandomBytes hashSaltLength
+  let _passwordHash = Crypto.fastPBKDF2_SHA256 params pwd _passwordHashSalt
+  return $ PasswordHash {..}
