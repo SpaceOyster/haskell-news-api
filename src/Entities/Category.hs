4750d1162a227ac34d45e812b2fdb348c0d1bc97
SpaceOyster <spaceoyster@disroot.org>
2022-09-20 00:46:01 +0500

    changed Entities.Category._categoryName type to (CI Text)

--- src/Entities/Category.hs
+++ src/Entities/Category.hs
@@ -6,11 +6,12 @@
 
 module Entities.Category where
 
+import Data.CaseInsensitive as CI
 import Data.Text
 import Database.Beam
 
 data CategoryT f = Category
-  { _categoryName :: Columnar f Text,
+  { _categoryName :: Columnar f (CI Text),
     _categoryParentCategory :: PrimaryKey CategoryT (Nullable f)
   }
   deriving (Generic, Beamable)
@@ -22,7 +23,7 @@ deriving instance Show Category
 deriving instance Eq Category
 
 instance Table CategoryT where
-  data PrimaryKey CategoryT f = CategoryId (Columnar f Text)
+  data PrimaryKey CategoryT f = CategoryId (Columnar f (CI Text))
     deriving (Generic, Beamable)
   primaryKey = CategoryId . _categoryName
 
