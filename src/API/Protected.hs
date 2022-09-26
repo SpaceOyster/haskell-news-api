e07a2a9db2be8445fbef384cfd43976faf9e4b79
SpaceOyster <spaceoyster@disroot.org>
2022-09-26 22:52:47 +0500

    updated API.Protected module to match last commit

--- src/API/Protected.hs
+++ src/API/Protected.hs
@@ -9,6 +9,7 @@ import DB
 import Data.CaseInsensitive as CI (mk)
 import Data.Text.Encoding (decodeUtf8)
 import Database.Beam
+import qualified Effects.Database as DB
 import Entities.User
 import qualified Handlers.Database as DB
 import Network.Wai
@@ -18,24 +19,23 @@ import Servant.Server.Internal.BasicAuth
 
 type instance AuthServerData (AuthProtect "basic-auth") = User
 
-lookupAccount :: AppEnv -> BasicAuthData -> Handler User
-lookupAccount env basicAuthData = do
+lookupAccount :: BasicAuthData -> App User
+lookupAccount basicAuthData = do
   let username = CI.mk $ decodeUtf8 (basicAuthUsername basicAuthData)
-  let password = basicAuthPassword basicAuthData
+  let pass = basicAuthPassword basicAuthData
   maybeUser <-
-    liftIO
-      . DB.runDBQuery (envDatabase env)
+    DB.runQuery
       . runSelectReturningOne
       . select
       . filter_ (\u -> _userLogin u ==. val_ username)
       $ all_ (_newsUsers newsDB)
   case maybeUser of
     Nothing -> throwError err404
-    Just user -> if checkPassword password user then pure user else throwError err404
+    Just user -> if checkPassword pass user then pure user else throwError err404
 
 authHandler :: AppEnv -> AuthHandler Request User
 authHandler env = mkAuthHandler handler
   where
     handler req = do
       let maybeBasicAuthData = decodeBAHdr req
-      maybe (throwError err404) (lookupAccount env) maybeBasicAuthData
+      maybe (throwError err404) (appToHandler env . lookupAccount) maybeBasicAuthData
