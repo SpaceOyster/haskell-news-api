7c25f870557fec88c1e46da04e7224ce2ef9042c
SpaceOyster <spaceoyster@disroot.org>
2022-09-26 22:52:05 +0500

    App.Monad App is now wrapped around Servant.Handler

--- src/App/Monad.hs
+++ src/App/Monad.hs
@@ -6,6 +6,7 @@ module App.Monad where
 
 import App.Env (Env (envConfig, envDatabase, envLogger))
 import Control.Monad.Catch (MonadCatch, MonadThrow)
+import Control.Monad.Except
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Reader (MonadReader, ReaderT (..), asks)
 import Effects.Config as Config
@@ -13,12 +14,12 @@ import Effects.Database as DB
 import Effects.Log as Log
 import qualified Handlers.Database as DB
 import Handlers.Logger as Logger
-import Servant (Handler)
+import Servant (Handler, ServerError, runHandler)
 
 type AppEnv = Env App
 
 newtype App a = App
-  { unApp :: ReaderT AppEnv IO a
+  { unApp :: ReaderT AppEnv Handler a
   }
   deriving newtype
     ( Functor,
@@ -27,14 +28,15 @@ newtype App a = App
       MonadIO,
       MonadThrow,
       MonadCatch,
-      MonadReader AppEnv
+      MonadReader AppEnv,
+      MonadError ServerError
     )
 
-runApp :: App a -> AppEnv -> IO a
-runApp app = runReaderT (unApp app)
+runApp :: App a -> AppEnv -> IO (Either ServerError a)
+runApp app = runHandler . runReaderT (unApp app)
 
 appToHandler :: AppEnv -> App a -> Handler a
-appToHandler env = liftIO . flip runApp env
+appToHandler env = flip runReaderT env . unApp
 
 instance Log.MonadLog App where
   doLog p t = do
