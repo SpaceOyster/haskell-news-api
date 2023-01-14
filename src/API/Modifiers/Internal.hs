bab94e7ae7440a69b8a9cba2d05a7a1000c4abf1
SpaceOyster <spaceoyster@disroot.org>
2023-01-14 13:26:58 +0500

    added API.Modifiers.Internal.TaggedColumn and ColumnList types

--- src/API/Modifiers/Internal.hs
+++ src/API/Modifiers/Internal.hs
@@ -1,5 +1,29 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 module API.Modifiers.Internal where
 
 import GHC.TypeLits
 
 data Tagged t = Tagged Symbol t
+
+newtype TaggedColumn (tag :: Symbol) be s a = TaggedCol
+  { unTaggedCol :: QExpr be s a
+  }
+
+data ColumnList be s a where
+  ColNil :: ColumnList be s '[]
+  ColCons :: TaggedColumn tag be s a -> ColumnList be s as -> ColumnList be s ('Tagged tag a ': as)
+
+(.:.) :: TaggedColumn tag be s a -> ColumnList be s as -> ColumnList be s ('Tagged tag a : as)
+(.:.) = ColCons
+
+infixr 3 .:.
+
+infixr 3 `ColCons`
