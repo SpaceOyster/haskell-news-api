882d5c3921076f5d9e55239e48cb46eb1d9a577f
SpaceOyster <spaceoyster@disroot.org>
2023-03-01 17:56:20 +0500

    vastly reworked API.Modifiers.Beam.Sortable module to exclude dirty `fromJust` trick

--- src/API/Modifiers/Beam/Sortable.hs
+++ src/API/Modifiers/Beam/Sortable.hs
@@ -1,15 +1,24 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module API.Modifiers.Beam.Sortable where
 
 import API.Modifiers.Internal
 import API.Modifiers.Sortable
-import Data.Maybe (fromJust)
+import Data.Maybe (fromMaybe)
+import Data.Typeable (Proxy (..))
 import Data.Void
 import Database.Beam (asc_, desc_, orderBy_)
 import Database.Beam.Backend.SQL (BeamSqlBackend)
-import Database.Beam.Query (QExpr, SqlOrderable)
+import Database.Beam.Query (QExpr)
 import Database.Beam.Query.Internal
   ( Projectible,
     Q,
@@ -33,21 +42,43 @@ newtype SortingApp be s sortspec = SortingApp
   { unSortingApp :: ColumnList be s sortspec
   }
 
-sortBy_ ::
+composeBeamOrdering ::
+  forall be be' s s' sortspec available deflt.
   ( BeamSqlBackend be,
     BeamSqlBackend be',
+    ReifySorting deflt,
+    LookupColumn be' s' (ColumnList be s sortspec),
+    ObtainColumn be' s' (ColumnList be s sortspec) (UnSorting deflt),
+    HasToBeProvided (UnSorting deflt) (ColumnList be s sortspec)
+  ) =>
+  SortingApp be s sortspec ->
+  SortingRequest available deflt ->
+  QOrd be' s' Void
+composeBeamOrdering (SortingApp colList) sreq =
+  fromMaybe (defaultOrder defaultColumn) mOrd
+  where
+    sorting = unSortingRequest sreq
+    colName = unSorting sorting
+    sortingOrder = sortingOrder_ sorting
+    mColumn = lookupColumn colList colName
+    mOrd = sortingOrder <$> mColumn
+    defaultSorting = reifySorting @deflt
+    defaultOrder = sortingOrder_ defaultSorting
+    defaultColumn = obtainColumn colList (Proxy :: Proxy (UnSorting deflt))
+
+sortBy_ ::
+  ( BeamSqlBackend be,
     Projectible be a,
-    SqlOrderable be (QOrd be' s' Void),
     ThreadRewritable (QNested s) a,
+    ReifySorting deflt,
+    HasToBeSubset available (ListOfTags sortspec),
+    SortingHasToBeAvailable deflt available,
     LookupColumn be (QNested s) (ColumnList be (QNested s) sortspec),
-    HasToBeSubset available (ListOfTags sortspec)
+    ObtainColumn be (QNested s) (ColumnList be (QNested s) sortspec) (UnSorting deflt),
+    HasToBeProvided (UnSorting deflt) (ColumnList be (QNested s) sortspec)
   ) =>
   SortingRequest available deflt ->
   (a -> SortingApp be (QNested s) sortspec) ->
   Q be db (QNested s) a ->
   Q be db s (WithRewrittenThread (QNested s) s a)
-sortBy_ (SortingRequest sorting) sortApp = orderBy_ $ \a ->
-  let colList = unSortingApp $ sortApp a
-      colName = unSorting sorting
-      sortingOrder = sortingOrder_ sorting
-   in sortingOrder (fromJust $ lookupColumn colList colName)
+sortBy_ sreq sortApp = orderBy_ $ \a -> composeBeamOrdering (sortApp a) sreq
