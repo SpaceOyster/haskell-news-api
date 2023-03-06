2191bed9fa3bc19532ef251273f1bd4c2f4d533b
SpaceOyster <spaceoyster@disroot.org>
2023-03-07 00:25:59 +0500

    added API.Modifiers.Internal.Tagged.UnTagged type family

--- src/API/Modifiers/Internal/Tagged.hs
+++ src/API/Modifiers/Internal/Tagged.hs
@@ -10,6 +10,9 @@ data Tagged t = Tagged Symbol t
 
 type (:?) = 'Tagged
 
+type family UnTagged a where
+  UnTagged ('Tagged _ a) = a
+
 type family ListOfTags (a :: [Tagged Type]) :: [Symbol] where
   ListOfTags '[] = '[]
   ListOfTags ('Tagged tag a ': as) = tag ': ListOfTags as
