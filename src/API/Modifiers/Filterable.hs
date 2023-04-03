3d7bfd7f068f7d07bf6c5b0faccf4604e811f11b
SpaceOyster <spaceoyster@disroot.org>
2023-04-03 19:22:20 +0500

    added comments to each of new API.Modifiers.Filterable type families

--- src/API/Modifiers/Filterable.hs
+++ src/API/Modifiers/Filterable.hs
@@ -111,30 +111,74 @@ data Filter (tag :: Symbol) a = Filter
 
 data FilterableBy' (tag :: Symbol) a
 
+-- | Used in generating allowed query params for filters.
+-- >>> ../user?registration-date_lt="01-01-2001"
 type PredicateSymbols :: [Symbol]
 type PredicateSymbols = ["eq", "lt", "gt", "neq", "nlt", "ngt", "gte", "lte"]
 
+-- | Generates list of Symbols by prepending specified tag to each Symbol of
+-- provided list, separating them with "_" symbol.
+-- >>> :k! PrependTagToSuffixes "name" ["1", "2"]
+-- PrependTagToSuffixes "name" ["1", "2"] :: [Symbol]
+-- = '["name_1", "name_2"]
 type family PrependTagToSuffixes (tag :: Symbol) (suffixes :: [Symbol]) :: [Symbol] where
   PrependTagToSuffixes tag '[] = '[]
   PrependTagToSuffixes tag (a ': as) =
     AppendSymbol tag (AppendSymbol "_" a) ': PrependTagToSuffixes tag as
 
+-- | Generates list of query param names for a 'filter' by prependig it's name
+-- to each of PredicateSymbols sepparating them with "_" symbol.
+-- >>> :k! GenerateFilterTags "filter"
+-- GenerateFilterTags "filter" :: [Symbol]
+-- = '["filter_eq", "filter_lt", "filter_gt", "filter_neq",
+--     "filter_nlt", "filter_ngt", "filter_gte", "filter_lte"]
 type family GenerateFilterTags (tag :: Symbol) :: [Symbol] where
   GenerateFilterTags tag = PrependTagToSuffixes tag PredicateSymbols
 
+-- | Generates a list of 'KnownSymbol' Constraint for each Symbol
+-- of 'GenerateFilterTags tag' list and concatenates them.
+-- >>> :k! GeneratedTagsAreKnownSymbol "filter"
+-- GeneratedTagsAreKnownSymbol "filter" :: Constraint
+-- = (KnownSymbol "filter_eq",
+--    (KnownSymbol "filter_lt",
+--    ...
+--         (KnownSymbol "filter_gte",
+--          (KnownSymbol "filter_lte", () :: Constraint))))))))
 type family GeneratedTagsAreKnownSymbol (tag :: Symbol) :: Constraint where
   GeneratedTagsAreKnownSymbol tag =
     ConcatConstraints (Fmap KnownSymbol (GenerateFilterTags tag))
 
+-- | Generates a list of 'QueryParam' for list of Symbols and specified type.
+-- >>> :k! ApplyQueryParam ["one", "two"] Integer
+-- ApplyQueryParam ["one", "two"] Integer :: [*]
+-- = '[QueryParam "one" Integer, QueryParam "two" Integer]
 type family ApplyQueryParam (filterTags :: [Symbol]) typ where
   ApplyQueryParam '[] typ = '[]
   ApplyQueryParam (a ': as) typ =
     QueryParam a typ ': ApplyQueryParam as typ
 
+-- | Generates a list of 'QueryParam' for specified 'filterName' and type.
+-- >>> :k! GenerateFilterQueryParams "created-at" UTCTime
+-- GenerateFilterQueryParams "created-at" UTCTime :: [*]
+-- = '[QueryParam "created-at_eq" UTCTime,
+--     QueryParam "created-at_lt" UTCTime,
+--     ...
+--     QueryParam "created-at_gte" UTCTime,
+--     QueryParam "created-at_lte" UTCTime]
 type family GenerateFilterQueryParams (filterName :: Symbol) typ where
   GenerateFilterQueryParams filterName typ =
     ApplyQueryParam (GenerateFilterTags filterName) typ
 
+-- | Generates 'Servant' API type by generated a list of 'QueryParam',
+-- inteerspersing them with '(:>)' operator and appending ':> api' at the end.
+-- >>> :k! GenerateFilterAPIType "registered-at" UTCTime (Get '[JSON] User)
+-- GenerateFilterAPIType "registered-at" UTCTime (Get '[JSON] User) :: *
+-- = QueryParam' '[Optional, Strict] "registered-at_eq" UTCTime
+--   :> (QueryParam' '[Optional, Strict] "registered-at_lt" UTCTime
+--       :> (QueryParam' '[Optional, Strict] "registered-at_gt" UTCTime
+--       ...
+--         :> (QueryParam' '[Optional, Strict] "registered-at_lte" UTCTime
+--           :> Verb 'GET 200 '[JSON] User)))))))
 type family GenerateFilterAPIType (filterName :: Symbol) typ api where
   GenerateFilterAPIType filterName typ api =
     Foldr (:>) api (GenerateFilterQueryParams filterName typ)
