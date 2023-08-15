100f072157279cb3703fcc9abd805690a15020a2
SpaceOyster <spaceoyster@disroot.org>
2023-08-15 23:40:42 +0500

    added ToJSON instance for API.Routes.Categories.CategoryJSON

--- src/API/Routes/Categories.hs
+++ src/API/Routes/Categories.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeOperators #-}
 
 module API.Routes.Categories where
 
 import App.Monad
+import Data.Aeson as A
+import Data.CaseInsensitive as CI (CI (original), mk)
 import Data.Text
 import Entities.Category
 import Servant
@@ -12,6 +15,14 @@ import Servant
 type CategoriesAPI = Get '[JSON] Text :<|> PostCreated '[JSON] Text
 
 newtype CategoryJSON = CategoryJSON Category
+
+instance A.ToJSON CategoryJSON where
+  toJSON (CategoryJSON Category {..}) =
+    A.object $ ["name" A..= CI.original _categoryName] <> maybeParentField
+    where
+      catIdM = case _categoryParentCategory of
+        CategoryId idMaybe -> idMaybe
+      maybeParentField = maybe [] (\i -> ["parent" A..= CI.original i]) catIdM
 
 categories :: ServerT CategoriesAPI App
 categories = return "GET categories endpoint" :<|> return "POST categories endpoint"
