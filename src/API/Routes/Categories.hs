e9555968e248111e5346527e50eab7c2db7bdb17
SpaceOyster <spaceoyster@disroot.org>
2023-08-15 23:37:32 +0500

    added API.Routes.Categories.CategoryJSON newtype

--- src/API/Routes/Categories.hs
+++ src/API/Routes/Categories.hs
@@ -6,9 +6,12 @@ module API.Routes.Categories where
 
 import App.Monad
 import Data.Text
+import Entities.Category
 import Servant
 
 type CategoriesAPI = Get '[JSON] Text :<|> PostCreated '[JSON] Text
+
+newtype CategoryJSON = CategoryJSON Category
 
 categories :: ServerT CategoriesAPI App
 categories = return "GET categories endpoint" :<|> return "POST categories endpoint"
