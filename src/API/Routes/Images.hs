c2202dbbab9a69138239a0a596daf90d00f77b60
SpaceOyster <spaceoyster@disroot.org>
2023-09-04 22:57:52 +0500

    added `GET /images` implementation, needs polishing

--- src/API/Routes/Images.hs
+++ src/API/Routes/Images.hs
@@ -1,14 +1,68 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators #-}
 
 module API.Routes.Images where
 
 import App.Monad
-import Data.Text
+import Control.Monad.Except (MonadError)
+import Control.Monad.IO.Class (MonadIO)
+import DB
+import Data.ByteString as BS
+import Data.Maybe
+import Data.Text.Extended as T
+import Database.Beam
+import Effects.Database as DB
+import Effects.Log as Log
+import Entities.Image
 import Servant
 
-type ImagesAPI = Get '[JSON] Text :<|> PostCreated '[JSON] Text
+type ImagesAPI =
+  Capture "imageName" Text :> Get '[OctetStream] (Headers '[Header "Content-Type" String, Header "Content-Length" Integer] BS.ByteString)
+    :<|> PostCreated '[JSON] Text
 
 images :: ServerT ImagesAPI App
-images = return "GET categories endpoint" :<|> return "POST categories endpoint"
+images = getImage :<|> postImage
+
+-- TODO: 'Content-Type' header is duplicated
+getImage ::
+  ( DB.MonadDatabase m,
+    Log.MonadLog m,
+    MonadIO m,
+    MonadError ServerError m
+  ) =>
+  Text ->
+  m (Headers '[Header "Content-Type" String, Header "Content-Length" Integer] BS.ByteString)
+getImage imageName = do
+  (imageId, imageExt) <- parseImageName imageName
+  imgContentM <-
+    DB.runQuery
+      . runSelectReturningOne
+      . select
+      $ (_imageContent <$> all_ (_newsImages newsDB))
+  case imgContentM of
+    Nothing -> throwError err404
+    Just imgContent -> pure . addHeader (contMimeType imageExt) . addHeader (contLength imgContent) $ imgContent
+  where
+    contMimeType = imageExtensionToMimeType
+    contLength = fromIntegral . BS.length
+
+parseImageName ::
+  ( MonadLog m,
+    MonadError ServerError m
+  ) =>
+  Text ->
+  m (Text, Text)
+parseImageName imageName = case T.splitOn "." imageName of
+  [imageId, imageExt] -> return (imageId, imageExt)
+  _ -> Log.logInfo (T.tshow imageName <> " is invalid image name.") >> throwError err404
+
+imageExtensionToMimeType :: Text -> String
+imageExtensionToMimeType imageExt =
+  T.unpack $
+    "image/" <> case imageExt of
+      "jpg" -> "jpeg"
+      x -> x
+
+postImage multipartData = return "POST categories endpoint"
