dd57684cdc8e9e69cf28aa4d7b34cc7f2576301f
SpaceOyster <spaceoyster@disroot.org>
2023-09-26 00:04:01 +0500

    added `POST /images` route implementation

--- src/API/Routes/Images.hs
+++ src/API/Routes/Images.hs
@@ -2,27 +2,33 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 
 module API.Routes.Images where
 
+import API.Modifiers.Protected (Protected)
 import App.Monad
+import Control.Monad
 import Control.Monad.Except (MonadError)
 import Control.Monad.IO.Class (MonadIO)
 import DB
 import Data.Aeson as A
 import Data.ByteString as BS
-import Data.Maybe
+import Data.ByteString.Lazy as LBS (toStrict)
+import qualified Data.CaseInsensitive as CI
 import Data.Text.Extended as T
 import Database.Beam
 import Effects.Database as DB
 import Effects.Log as Log
 import Entities.Image
+import Entities.User
 import Servant
+import Servant.Multipart as MP
 
 type ImagesAPI =
   Capture "imageName" Text :> Get '[OctetStream] (Headers '[Header "Content-Type" String, Header "Content-Length" Integer] BS.ByteString)
-    :<|> PostCreated '[JSON] Text
+    :<|> Protected :> MultipartForm Mem (MultipartData Mem) :> PostCreated '[JSON] [ImageJSON]
 
 images :: ServerT ImagesAPI App
 images = getImage :<|> postImage
@@ -73,7 +79,6 @@ imageExtensionToMimeType imageExt =
       "jpg" -> "jpeg"
       x -> x
 
-postImage multipartData = return "POST categories endpoint"
 newtype ImageJSON = ImageJSON Image
 
 instance A.ToJSON ImageJSON where
@@ -85,3 +90,38 @@ instance A.ToJSON ImageJSON where
             "image-name" A..= _imageName
           ]
 
+postImage ::
+  ( Monad m,
+    MonadDatabase m,
+    MonadLog m,
+    MonadError ServerError m
+  ) =>
+  User ->
+  MultipartData Mem ->
+  m [ImageJSON]
+postImage usr multipartData =
+  if _userIsAdmin usr then doCreateImage else doOnUnauthorised
+  where
+    table = _newsCategories newsDB
+    creatorLogin = CI.original (_userLogin usr)
+    doCreateImage = do
+      let files = MP.files multipartData
+      newImgsData <- forM files fileToImageData
+      insertNewImages (_newsImages newsDB) newImgsData
+      imgs <- selectImages (_newsImages newsDB) . fmap newImageFileName $ newImgsData
+      return $ ImageJSON <$> imgs
+    fileToImageData file = do
+      let fileName = fdFileName file
+          newImageDataContent = LBS.toStrict $ fdPayload file
+          newImageMimeType = fdFileCType file
+      (fnName, fnExtension) <- parseImageFileName fileName
+      Log.logInfo $ "Form: " <> fdInputName file <> " Got file: " <> fileName
+      let newImageFileName = FileName {..}
+      return $ NewImage {..}
+    doOnUnauthorised = doLogUnauthorised >> throwError err401
+    doLogDBError fname =
+      Log.logWarning $
+        "Image \"" <> fname <> "\" was not added to Database"
+    doLogUnauthorised =
+      Log.logWarning $
+        "User \"" <> creatorLogin <> "\" is not authorised to create a new image"
