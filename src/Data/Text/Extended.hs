5a5663933892541751a9c71c9d6fb4a0dda4d6aa
SpaceOyster <spaceoyster@disroot.org>
2023-09-29 00:17:22 +0500

    added Data.Text.Extended.textToLBS function

--- src/Data/Text/Extended.hs
+++ src/Data/Text/Extended.hs
@@ -1,10 +1,16 @@
 module Data.Text.Extended
   ( module Data.Text,
     tshow,
+    textToLBS,
   )
 where
 
+import qualified Data.ByteString.Lazy as LBS (ByteString, fromStrict)
 import Data.Text
+import Data.Text.Encoding (encodeUtf8)
 
-tshow :: Show a => a -> Text
+tshow :: (Show a) => a -> Text
 tshow = pack . show
+
+textToLBS :: Text -> LBS.ByteString
+textToLBS = LBS.fromStrict . encodeUtf8
