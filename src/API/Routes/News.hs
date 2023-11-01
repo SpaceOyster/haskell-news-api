ad9cdb441ab6c02388914cf89266f52e77d76b1c
SpaceOyster <spaceoyster@disroot.org>
2023-11-02 01:27:28 +0500

    API.Routes.News.insertArticle Beam query (needs polishing)

--- src/API/Routes/News.hs
+++ src/API/Routes/News.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 
 module API.Routes.News where
@@ -16,11 +18,14 @@ import Data.CaseInsensitive as CI
 import Data.Int
 import Data.Text.Extended as T
 import Data.Time.Clock
+import Database.Beam
+import Database.Beam.Postgres
 import Effects.Database as DB
 import Effects.Log as Log
 import Entities.Category
 import Entities.Image
 import Entities.News
+import Entities.User
 import Servant
 
 type NewsAPI =
@@ -104,3 +109,53 @@ getArticle articleId = do
     doLogNotFound = Log.logInfo $ "Article " <> T.tshow articleId <> " not found"
 
 postArticle = undefined
+insertArticle ::
+  (MonadBeam Postgres m, MonadBeam Postgres m) =>
+  User ->
+  ArticlePostJSON ->
+  m (Maybe (ArticleT Identity))
+insertArticle creator (ArticlePostJSON {..}) = do
+  runInsert . insert (_newsArticles newsDB) $
+    insertExpressions
+      [ Article
+          { _articleId = default_,
+            _articleTitle = val_ _articlePostJSONTitle,
+            _articleCreatedAt = default_,
+            _articleAuthor = UserId (val_ $ _userId creator),
+            _articleCategory = CategoryId $ val_ (CI.mk <$> _articlePostJSONCategory),
+            _articleBody = val_ _articlePostJSONBody,
+            _articleIsPublished = val_ _articlePostJSONIsPublished
+          }
+      ]
+  article <-
+    runSelectReturningOne
+      . select
+      . filter_
+        ( \a ->
+            _articleTitle a
+              ==. val_ _articlePostJSONTitle
+              &&. _articleAuthor a
+              ==. UserId (val_ $ _userId creator)
+        )
+      $ all_ (_newsArticles newsDB)
+  mapM_ (insertArticleImagesRelations imgNames) article
+  pure article
+  where
+    imgNames = unFileNameJSON <$> _articlePostJSONImages
+
+insertArticleImagesRelations ::
+  (MonadBeam Postgres m) =>
+  [FileName] ->
+  Article ->
+  m ()
+insertArticleImagesRelations imgNames article =
+  runInsert . insert (_newsArticlesImages newsDB) . insertFrom $ do
+    img <-
+      filter_
+        (\i -> imgNameString i `in_` imgIdFileNames)
+        (all_ (_newsImages newsDB))
+    pure (ArticleImage (artPk article) (pk img))
+  where
+    imgIdFileNames = val_ . T.tshow <$> imgNames
+    imgNameString i = concat_ [cast_ (_imageId i) (varchar Nothing), val_ ".", _imageFileExtension i]
+    artPk art = ArticleId $ val_ $ _articleId art
