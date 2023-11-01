8d60df5a64f267ae4e1a13a5bc1a1d4a0f05c331
SpaceOyster <spaceoyster@disroot.org>
2023-11-02 01:16:18 +0500

    added API.Routes.News.FileNmaeJSON - helper newtype wrapper

--- src/API/Routes/News.hs
+++ src/API/Routes/News.hs
@@ -6,9 +6,21 @@ module API.Routes.News where
 
 import App.Monad
 import Data.Text
+import Data.Aeson as A
+import Data.Text.Extended as T
+import Entities.Image
 import Servant
 
 type NewsAPI = Get '[JSON] Text :<|> PostCreated '[JSON] Text
 
 news :: ServerT NewsAPI App
 news = return "GET categories endpoint" :<|> return "POST categories endpoint"
+
+newtype FileNameJSON = FileNameJSON {unFileNameJSON :: FileName}
+  deriving (Show)
+
+instance A.ToJSON FileNameJSON where
+  toJSON (FileNameJSON fn) = A.String $ "/images/" <> T.tshow fn
+
+instance A.FromJSON FileNameJSON where
+  parseJSON = A.withText "FileNameJSON" (fmap FileNameJSON . parseFileName)
