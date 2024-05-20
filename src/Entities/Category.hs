f14d90972286778f4031ff3d85195fdb836bcb9c
SpaceOyster <spaceoyster@disroot.org>
2024-05-20 22:54:24 +0500

    added Entities.Category.lookupCategoryWithDescendants function

--- src/Entities/Category.hs
+++ src/Entities/Category.hs
@@ -139,3 +139,17 @@ lookupCategoryIdWithAncestors table cId =
               (filter_ (\x -> pk x ==. CategoryId (val_ cId)) (all_ table))
               (reuse c >>= \c' -> filter_ (\x -> _categoryParentCategory c' ==. just_ (pk x)) (all_ table))
     pure (reuse c)
+
+lookupCategoryWithDescendants ::
+  (MonadDatabase m, MonadIO m, Database Postgres db, MonadBeam Postgres m) =>
+  DatabaseEntity Postgres db (TableEntity CategoryT) ->
+  CI Text ->
+  m [Category]
+lookupCategoryWithDescendants table catName =
+  runSelectReturningList . selectWith $ do
+    rec c <-
+          selecting $
+            union_
+              (filter_ (\x -> _categoryName x ==. val_ catName) (all_ table))
+              (reuse c >>= \c' -> filter_ (\x -> _categoryParentCategory x ==. just_ (pk c')) (all_ table))
+    pure (reuse c)
